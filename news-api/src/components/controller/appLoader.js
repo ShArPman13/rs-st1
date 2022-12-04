f0a87428ae47f432614fc4d83a4bd073aad70ff7
sharpman13 <sharpman2001@gmail.com>
2022-12-05 00:23:03 +0100

    refactor: add types in loader.js

--- news-api/src/components/controller/appLoader.js
+++ news-api/src/components/controller/appLoader.js
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.up.railway.app/', { //https://newsapi.org/v2/
             apiKey: 'f74f146e196247a0901b90a947ac928a', // получите свой ключ https://newsapi.org/
         });
     }
---
+++ news-api/src/components/controller/loader.ts
@@ -0,0 +1,54 @@
+interface IApiKey {
+  apiKey: string;
+}
+interface ISourceOfNews {
+  source: string;
+}
+
+type IUrlOptions = Record<string, string>
+
+interface IForGetResp {
+  endpoint: string;
+  options: ISourceOfNews;
+}
+
+class Loader {
+  constructor(public baseLink: string, public options: IApiKey) {
+    this.baseLink = baseLink;
+    this.options = options;
+  }
+
+  getResp({ endpoint, options }: IForGetResp, callback = () => { console.error('No callback for GET response'); }) {
+    this.load('GET', endpoint, callback, options);
+  }
+
+  errorHandler(res: Response) {
+    if (!res.ok) {
+      if (res.status === 401 || res.status === 404)
+        console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
+      throw Error(res.statusText);
+    }
+
+    return res;
+  }
+
+  makeUrl(options: ISourceOfNews, endpoint: string) {
+    const urlOptions: IUrlOptions = { ...this.options, ...options };
+    let url = `${this.baseLink}${endpoint}?`;
+
+    Object.keys(urlOptions).forEach((key) => {
+      url += `${key}=${urlOptions[key]}&`;
+    });
+    return url.slice(0, -1);
+  }
+
+  load(method: string, endpoint: string, callback: (firstArg: string) => void, options: ISourceOfNews) {
+    fetch(this.makeUrl(options, endpoint), { method })
+      .then(this.errorHandler)
+      .then((res) => res.json())
+      .then((data) => callback(data))
+      .catch((err) => console.error(err));
+  }
+}
+
+export default Loader;
