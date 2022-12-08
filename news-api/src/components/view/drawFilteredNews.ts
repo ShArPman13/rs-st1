46092554b562bb3f89ef273f4149936b67ebf9f0
sharpman13 <sharpman2001@gmail.com>
2022-12-08 16:09:51 +0100

    fix: add toLowerCase() to search.value in drawFilteredNews.ts for proper search

--- news-api/src/components/view/drawFilteredNews.ts
+++ news-api/src/components/view/drawFilteredNews.ts
@@ -4,7 +4,7 @@ import Sources from "./sources/sources";
 export const drawFilteredNews = (data: ISourcesNews[], sources: Sources) => {
   const search = <HTMLInputElement>document.querySelector('.text-field__input');
   search?.addEventListener('input', (e) => {
-    const filteredData = data.filter(el => el.name.toLowerCase().includes(search.value));
+    const filteredData = data.filter(el => el.name.toLowerCase().includes(search.value.toLowerCase()));
     sources.draw(filteredData);
   })
 }
